9c341c4b2ef165e61268d75a4c13d03b824e557b
Niraj Mallick <261571807+nirajmallick641-dot@users.noreply.github.com>
2026-05-10 11:27:40 +0530

    Update firebase-config.example.js

--- firebase-config.example.js
+++ firebase-config.example.js
@@ -1,8 +1,8 @@
 window.TRAVELOOP_FIREBASE_CONFIG = {
-  apiKey: "YOUR_API_KEY",
-  authDomain: "YOUR_PROJECT.firebaseapp.com",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_PROJECT.appspot.com",
-  messagingSenderId: "YOUR_SENDER_ID",
-  appId: "YOUR_APP_ID"
+  apiKey: "AIzaSyCCpBlFuClfdtoHN1PIvgz6jApJ2XzUVsg",
+  authDomain: "traveloop-820a3.firebaseapp.com",
+  projectId: "traveloop-820a3",
+  storageBucket: "traveloop-820a3.firebasestorage.app",
+  messagingSenderId: "815832276105",
+  appId: "1:815832276105:web:c2efc1fa09a6c112993a58"
 };
